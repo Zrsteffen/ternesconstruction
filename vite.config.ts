34bbd5f49135af75f0250eb56f42bbda4e088038
Zrsteffen <sandyfire312@gmail.com>
2025-05-04 08:01:36 -0500

    Update vite.config.ts
    logo fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  assetsInclude: ['**/*.JPG']
+  plugins: [react()],
+  assetsInclude: ['**/*.JPG'],
 });
