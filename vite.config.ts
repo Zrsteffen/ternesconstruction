3d3ff9f27e66a902314fdb87c2e002062fd4581e
Zrsteffen <sandyfire312@gmail.com>
2025-05-04 08:27:57 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.JPG'],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
