6f1fa6fad4dbd765596c550054787c14c2ed59ad
Zrsteffen <sandyfire312@gmail.com>
2025-05-07 21:36:48 -0500

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -137,7 +137,7 @@ const HomePage: React.FC = () => {
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-10">
             {[
               {
-                title: 'Modern Farmhouse',
+                title: 'Fully Customized Home',
                 category: 'Custom Home',
                 location: 'East Wichita',
                 image: modernFarmhouseHero,
