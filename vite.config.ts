a2f341df84673f876f2cf93073965adc43331aae
Zrsteffen <sandyfire312@gmail.com>
2025-05-09 12:31:22 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  base: './', // ✅ Add this line
+  base: './', // ✅ critical for GitHub Pages / GoDaddy deployments
   plugins: [react()],
   resolve: {
     alias: {
